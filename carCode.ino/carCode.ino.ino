6e5c32bc5ce6eca87574fd0f710d033bfb3b0bad
jwb <jared.briskman@students.olin.edu>
2017-10-11 19:59:17 -0400

    fixes cardinality in car code

--- carCode.ino/carCode.ino.ino
+++ carCode.ino/carCode.ino.ino
@@ -3,15 +3,15 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 // Define sensors, variables, and motors
-const int leftSensor = A0;
-const int rightSensor = A1;
+const int portSensor = A0;
+const int starboardSensor = A1;
 int sensorLeftValue = 0;
 int sensorRightValue = 0;
 int motorLeftValue = 0;
 int motorRightValue = 0;
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *portMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *starboardMotor = AFMS.getMotor(2);
 
 int counter = 0;
 
@@ -25,24 +25,24 @@ void loop() {
   // put your main code here, to run repeatedly:
   // read the sensor values
   if (counter < 5000) {
-    sensorLeftValue = analogRead(leftSensor);
-    sensorRightValue = analogRead(rightSensor);
+    sensorLeftValue = analogRead(portSensor);
+    sensorRightValue = analogRead(starboardSensor);
     // map the sensor value to motor values
     motorLeftValue = map(sensorLeftValue, 100, 300, 0, 150);
     motorRightValue = map(sensorRightValue, 100, 300, 0, 150);
     // set the motor values
-    leftMotor->setSpeed(motorLeftValue);
-    rightMotor->setSpeed(motorRightValue);
+    portMotor->setSpeed(motorLeftValue);
+    starboardMotor->setSpeed(motorRightValue);
     // run the motors
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
+    portMotor->run(FORWARD);
+    starboardMotor->run(FORWARD);
     delay(2);
     counter++;
   } else {
-    leftMotor->setSpeed(0);
-    rightMotor->setSpeed(0);
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
+    portMotor->setSpeed(0);
+    starboardMotor->setSpeed(0);
+    portMotor->run(FORWARD);
+    starboardMotor->run(FORWARD);
   }
   
 }
