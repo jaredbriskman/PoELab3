62fc3dcfd21a2c6c6b3fb39e943f02e8261f9615
jwb <jared.briskman@students.olin.edu>
2017-10-11 19:46:00 -0400

    working test code

--- test/test.ino
+++ test/test.ino
@@ -15,8 +15,8 @@ void loop() {
   sSenseValue = analogRead(sSensePin);
   // print the results to the Serial Monitor:
   Serial.print(pSenseValue);
-  Serial.print('x')
-  Serial.print(sSenseValue)
+  Serial.print('x');
+  Serial.println(sSenseValue);
   // wait 2 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(2);
