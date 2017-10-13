0121ffe570fcce5a82888e9e4a829261cb5fb2f4
jwb <jared.briskman@students.olin.edu>
2017-10-13 01:09:35 -0400

    Adds logging to carcode

--- carCode/carCode.ino
+++ carCode/carCode.ino
@@ -14,12 +14,19 @@ Adafruit_DCMotor *portMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *starboardMotor = AFMS.getMotor(2);
 
 int counter = 0;
+bool record = false;
 
 void setup() {
   // put your setup code here, to run once:
   // start the controller
   AFMS.begin();
   Serial.begin(9600);
+  Serial.println ("Log? (y/n):");
+  while(Serial.available() == 0){}
+  buffer = Serial.read();
+  if (buffer == 'y') {
+      record = true;
+  }
 }
 
 void loop() {
@@ -30,27 +37,38 @@ void loop() {
   // map the sensor value to motor values
   portMotor->setSpeed(30);
   starboardMotor->setSpeed(30);
-  Serial.print(sensorPortValue);
-  Serial.print(",");
-  Serial.print(sensorStarboardValue);
-  Serial.print(",");
+  if (record) {
+      Serial.print(sensorPortValue);
+      Serial.print(",");
+      Serial.print(sensorStarboardValue);
+      Serial.print(",");
+    }
   if (sensorPortValue > 800) {
       portMotor->run(BACKWARD);
+      if (record) {
       Serial.print("-1");
       }
+      }
   else {portMotor->run(FORWARD);
+      if (record) {
         Serial.print("1");
+        Serial.print(",");
         }
-  Serial.print(",");
+        }
   
   if (sensorStarboardValue > 800) {
       starboardMotor->run(BACKWARD);
+      if (record) {
       Serial.print("-1");
-  }
+}  
+}
   
   else {starboardMotor->run(FORWARD);
-        Serial.print("1");}
-  Serial.println();
+      if (record) {
+        Serial.print("1");
+        Serial.println();
+}
+}
   /*
   motorPortValue = map(sensorPortValue, 0, 1024, -65, 65);
   motorStarboardValue = map(sensorStarboardValue, 0, 1024, -65, 65);
