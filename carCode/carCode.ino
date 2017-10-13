b8eb35548a594f9eef6178a483cecffcc54d267a
Ian Paul <ian.paul@students.olin.edu>
2017-10-13 01:20:28 -0400

    Jared add serial code

--- carCode/carCode.ino
+++ carCode/carCode.ino
@@ -9,12 +9,13 @@ int sensorPortValue = 0;
 int sensorStarboardValue = 0;
 int motorPortValue = 0;
 int motorStarboardValue = 0;
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *portMotor = AFMS.getMotor(1);
 Adafruit_DCMotor *starboardMotor = AFMS.getMotor(2);
 
 int counter = 0;
 bool record = false;
+char buffer;
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,10 +23,11 @@ void setup() {
   AFMS.begin();
   Serial.begin(9600);
   Serial.println ("Log? (y/n):");
-  while(Serial.available() == 0){}
+  while (Serial.available() == 0) {}
   buffer = Serial.read();
+  char Str[2] = {buffer};
   if (buffer == 'y') {
-      record = true;
+    record = true;
   }
 }
 
@@ -38,45 +40,47 @@ void loop() {
   portMotor->setSpeed(30);
   starboardMotor->setSpeed(30);
   if (record) {
-      Serial.print(sensorPortValue);
-      Serial.print(",");
-      Serial.print(sensorStarboardValue);
+    Serial.print(sensorPortValue);
+    Serial.print(",");
+    Serial.print(sensorStarboardValue);
+    Serial.print(",");
+  }
+  if (sensorPortValue > 800) {
+    portMotor->run(BACKWARD);
+    if (record) {
+      Serial.print("-1");
+    }
+  }
+  else {
+    portMotor->run(FORWARD);
+    if (record) {
+      Serial.print("1");
       Serial.print(",");
     }
-  if (sensorPortValue > 800) {
-      portMotor->run(BACKWARD);
-      if (record) {
-      Serial.print("-1");
-      }
-      }
-  else {portMotor->run(FORWARD);
-      if (record) {
-        Serial.print("1");
-        Serial.print(",");
-        }
-        }
-  
+  }
+
   if (sensorStarboardValue > 800) {
-      starboardMotor->run(BACKWARD);
-      if (record) {
+    starboardMotor->run(BACKWARD);
+    if (record) {
       Serial.print("-1");
-}  
-}
-  
-  else {starboardMotor->run(FORWARD);
-      if (record) {
-        Serial.print("1");
-        Serial.println();
-}
-}
+    }
+  }
+
+  else {
+    starboardMotor->run(FORWARD);
+    if (record) {
+      Serial.print("1");
+      Serial.println();
+    }
+  }
   /*
-  motorPortValue = map(sensorPortValue, 0, 1024, -65, 65);
-  motorStarboardValue = map(sensorStarboardValue, 0, 1024, -65, 65);
-  // set the motor values
-  portMotor->setSpeed(abs(motorPortValue));
-  starboardMotor->setSpeed(abs(motorStarboardValue));
-  // run the motors
-  portMotor->run(motorPortValue < 0 ? BACKWARD: FORWARD);
-  starboardMotor->run(motorStarboardValue < 0 ? BACKWARD: FORWARD);
+    motorPortValue = map(sensorPortValue, 0, 1024, -65, 65);
+    motorStarboardValue = map(sensorStarboardValue, 0, 1024, -65, 65);
+    // set the motor values
+    portMotor->setSpeed(abs(motorPortValue));
+    starboardMotor->setSpeed(abs(motorStarboardValue));
+    // run the motors
+    portMotor->run(motorPortValue < 0 ? BACKWARD: FORWARD);
+    starboardMotor->run(motorStarboardValue < 0 ? BACKWARD: FORWARD);
   */
 }
